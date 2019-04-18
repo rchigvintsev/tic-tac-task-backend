a3c8fb4b28d8042200798b554b9cc6595b6aad8e
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-04-18 11:54:29 +0300

    Temporarily disable security

--- src/main/java/org/briarheart/orchestra/OrchestraApplication.java
+++ src/main/java/org/briarheart/orchestra/OrchestraApplication.java
@@ -3,9 +3,12 @@ package org.briarheart.orchestra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class OrchestraApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(OrchestraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestraApplication.class, args);
+    }
 }
