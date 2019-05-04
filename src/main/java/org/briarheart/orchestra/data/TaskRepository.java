874931e0c281625f8d869e5f4c5c82525600ac3b
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-05-04 17:18:21 +0300

    Add way to filter tasks by 'completed' field

--- src/main/java/org/briarheart/orchestra/data/TaskRepository.java
+++ src/main/java/org/briarheart/orchestra/data/TaskRepository.java
@@ -1,9 +1,13 @@
 package org.briarheart.orchestra.data;
 
 import org.briarheart.orchestra.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Page<Task> findByCompleted(@Param("completed") Boolean completed, Pageable pageable);
 }
