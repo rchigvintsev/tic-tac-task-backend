36d23ea157e7d776798e19ebb415fd434366037c
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-06-01 20:23:30 +0300

    Expose ids for TaskComment domain model

--- src/main/java/org/briarheart/orchestra/config/RepositoryRestConfig.java
+++ src/main/java/org/briarheart/orchestra/config/RepositoryRestConfig.java
@@ -1,6 +1,7 @@
 package org.briarheart.orchestra.config;
 
 import org.briarheart.orchestra.model.Task;
+import org.briarheart.orchestra.model.TaskComment;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,6 +10,6 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 public class RepositoryRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Task.class);
+        config.exposeIdsFor(Task.class, TaskComment.class);
     }
 }
