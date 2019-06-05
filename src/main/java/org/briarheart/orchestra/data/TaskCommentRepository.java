0e543d550eea1c547dd9eb392b5182c43b61629b
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-06-05 10:53:00 +0300

    Sort comments by 'createdAt' field in descending order

--- src/main/java/org/briarheart/orchestra/data/TaskCommentRepository.java
+++ src/main/java/org/briarheart/orchestra/data/TaskCommentRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author Roman Chigvintsev
  */
 @RepositoryRestResource
 public interface TaskCommentRepository extends CrudRepository<TaskComment, Long> {
-    Page<TaskComment> findByTaskId(@Param("taskId") Long taskId, Pageable pageable);
+    @RestResource(path = "findByTaskId")
+    Page<TaskComment> findByTaskIdOrderByCreatedAtDesc(@Param("taskId") Long taskId, Pageable pageable);
 }
