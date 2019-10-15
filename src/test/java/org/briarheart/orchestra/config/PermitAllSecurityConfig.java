cecd023c262a322e53647ed17f39187c33dcbcf4
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-10-16 01:35:34 +0300

    Enable security in integration tests when 'security.disabled' property is set to false

--- src/test/java/org/briarheart/orchestra/config/PermitAllSecurityConfig.java
+++ src/test/java/org/briarheart/orchestra/config/PermitAllSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.briarheart.orchestra.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -7,10 +8,13 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * Security configuration that permits all requests.
+ * <p>
+ * To exclude this configuration from integration tests set system property "security.disabled" to {@code false}.
  *
  * @author Roman Chigvintsev
  */
 @EnableWebFluxSecurity
+@ConditionalOnProperty(name = "security.disabled", havingValue = "true", matchIfMissing = true)
 public class PermitAllSecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
