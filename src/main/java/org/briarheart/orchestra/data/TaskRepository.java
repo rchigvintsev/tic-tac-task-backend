101c347c7d7752f61918705fecf0e741c8a52174
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-12-05 01:34:03 +0300

    Adapt to new version of Spring Data R2DBC

--- src/main/java/org/briarheart/orchestra/data/TaskRepository.java
+++ src/main/java/org/briarheart/orchestra/data/TaskRepository.java
@@ -1,7 +1,7 @@
 package org.briarheart.orchestra.data;
 
 import org.briarheart.orchestra.model.Task;
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
