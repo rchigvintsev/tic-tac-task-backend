a96f4f4b91e3a3cce997605055b04be06aa90561
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-02-13 13:35:39 +0300

    Introduce custom 'ClockProvider' for validation of attributes containing date/time values in UTC time zone

---
+++ src/main/java/org/briarheart/orchestra/validation/UtcClockProvider.java
@@ -0,0 +1,22 @@
+package org.briarheart.orchestra.validation;
+
+import javax.validation.ClockProvider;
+import java.time.Clock;
+
+/**
+ * Implementation of {@link ClockProvider} which returns the current system time in UTC time zone using
+ * {@link Clock#systemUTC()}.
+ *
+ * @author Roman Chigvintsev
+ */
+public class UtcClockProvider implements ClockProvider {
+    /**
+     * Returns the current system time in UTC time zone.
+     *
+     * @return instance of {@link Clock} in UTC time zone
+     */
+    @Override
+    public Clock getClock() {
+        return Clock.systemUTC();
+    }
+}
