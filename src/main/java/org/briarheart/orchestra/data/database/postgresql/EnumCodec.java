91378f4164a63a06cd52624d742c58d6c89e55e3
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-03-27 02:33:02 +0300

    Add custom PostgreSQL codec to represent enumeration values as text in database

---
+++ src/main/java/org/briarheart/orchestra/data/database/postgresql/EnumCodec.java
@@ -0,0 +1,83 @@
+package org.briarheart.orchestra.data.database.postgresql;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.r2dbc.postgresql.client.Parameter;
+import io.r2dbc.postgresql.codec.Codec;
+import io.r2dbc.postgresql.message.Format;
+import io.r2dbc.postgresql.type.PostgresqlObjectId;
+import io.r2dbc.postgresql.util.ByteBufUtils;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Mono;
+
+import static io.r2dbc.postgresql.message.Format.FORMAT_TEXT;
+import static io.r2dbc.postgresql.type.PostgresqlObjectId.*;
+
+/**
+ * Codec that encodes/decodes {@link Enum}s using {@link Enum#name()} method to represent enumeration value as text
+ * in database.
+ *
+ * @author Roman Chigvintsev
+ */
+@RequiredArgsConstructor
+public class EnumCodec implements Codec<Enum<?>> {
+    @NonNull
+    private final ByteBufAllocator byteBufAllocator;
+
+    @Override
+    public boolean canDecode(int dataType, Format format, Class<?> type) {
+        Assert.notNull(type, "Type must not be null");
+
+        if (PostgresqlObjectId.isValid(dataType) && (type == Object.class || type.isEnum())) {
+            PostgresqlObjectId objectId = valueOf(dataType);
+            return BPCHAR == objectId
+                    || CHAR == objectId
+                    || TEXT == objectId
+                    || UNKNOWN == objectId
+                    || VARCHAR == objectId;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean canEncode(Object value) {
+        Assert.notNull(value, "Value must not be null");
+        return value.getClass().isEnum();
+    }
+
+    @Override
+    public boolean canEncodeNull(Class<?> type) {
+        Assert.notNull(type, "Type must not be null");
+        return type.isEnum();
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    public Enum<?> decode(ByteBuf buffer, int dataType, Format format, Class<? extends Enum<?>> type) {
+        if (buffer == null) {
+            return null;
+        }
+        Assert.notNull(type, "Type must not be null");
+        return Enum.valueOf((Class<? extends Enum>) type, ByteBufUtils.decode(buffer));
+    }
+
+    @Override
+    public Parameter encode(Object value) {
+        Assert.notNull(value, "Value must not be null");
+        return new Parameter(FORMAT_TEXT, VARCHAR.getObjectId(), Mono.fromSupplier(()
+                -> ByteBufUtils.encode(byteBufAllocator, ((Enum<?>) value).name())));
+    }
+
+    @Override
+    public Parameter encodeNull() {
+        return new Parameter(FORMAT_TEXT, VARCHAR.getObjectId(), Parameter.NULL_VALUE);
+    }
+
+    @Override
+    public Class<?> type() {
+        return Enum.class;
+    }
+}
