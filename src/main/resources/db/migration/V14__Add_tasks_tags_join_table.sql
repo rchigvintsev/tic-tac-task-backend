cee4f4ed6181759e182198c3087341ebcec801c7
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-09-09 23:51:30 +0300

    Fix names of 'tasks_tags' table constraints

--- src/main/resources/db/migration/V14__Add_tasks_tags_join_table.sql
+++ src/main/resources/db/migration/V14__Add_tasks_tags_join_table.sql
@@ -1,7 +1,8 @@
+-- noinspection SqlResolve
 CREATE TABLE tasks_tags (
   task_id BIGINT,
   tag_id BIGINT,
-  CONSTRAINT pk_tasks_task_tags PRIMARY KEY (task_id, tag_id),
-  CONSTRAINT fk_tasks_task_tags_task FOREIGN KEY (task_id) REFERENCES task(id),
-  CONSTRAINT fk_tasks_task_tags_task_tag FOREIGN KEY (tag_id) REFERENCES tag(id)
+  CONSTRAINT pk_tasks_tags PRIMARY KEY (task_id, tag_id),
+  CONSTRAINT fk_tasks_tags_task FOREIGN KEY (task_id) REFERENCES task(id),
+  CONSTRAINT fk_tasks_tags_tag FOREIGN KEY (tag_id) REFERENCES tag(id)
 )
