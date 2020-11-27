87da879a7fe9cfeefa1877b495e51ee7c7b27072
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-11-27 14:17:29 +0300

    Fix CLOB-to-string H2 codec

--- src/test/java/org/briarheart/orchestra/config/TestR2dbcConnectionFactoryConfig.java
+++ src/test/java/org/briarheart/orchestra/config/TestR2dbcConnectionFactoryConfig.java
@@ -1,8 +1,7 @@
 package org.briarheart.orchestra.config;
 
 import io.r2dbc.h2.CustomH2ConnectionFactory;
-import io.r2dbc.h2.codecs.DefaultCodecs;
-import io.r2dbc.h2.codecs.DelegatingExternalCodecs;
+import io.r2dbc.h2.codecs.CustomCodecs;
 import io.r2dbc.h2.codecs.EnumCodec;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +17,7 @@ public class TestR2dbcConnectionFactoryConfig {
     @Bean
     public ConnectionFactory connectionFactory() {
         return CustomH2ConnectionFactory.inMemory("testdb")
-                .withCodecsProvider(client -> {
-                    DefaultCodecs delegate = new DefaultCodecs(client);
-                    return new DelegatingExternalCodecs(delegate, List.of(new EnumCodec()));
-                })
+                .withCodecsProvider(client -> new CustomCodecs(client, List.of(new EnumCodec())))
                 .build();
     }
 }
