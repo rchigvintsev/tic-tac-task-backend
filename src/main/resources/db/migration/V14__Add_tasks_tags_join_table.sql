4e54c520264c7e5991b851f9fd02ef01129a282d
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-12-31 23:49:16 +0300

    Enable cascade deletion of task comments and task-tag relations

--- src/main/resources/db/migration/V14__Add_tasks_tags_join_table.sql
+++ src/main/resources/db/migration/V14__Add_tasks_tags_join_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE tasks_tags (
   task_id BIGINT,
   tag_id BIGINT,
   CONSTRAINT pk_tasks_tags PRIMARY KEY (task_id, tag_id),
-  CONSTRAINT fk_tasks_tags_task FOREIGN KEY (task_id) REFERENCES task(id),
-  CONSTRAINT fk_tasks_tags_tag FOREIGN KEY (tag_id) REFERENCES tag(id)
+  CONSTRAINT fk_tasks_tags_task FOREIGN KEY (task_id) REFERENCES task(id) ON DELETE CASCADE,
+  CONSTRAINT fk_tasks_tags_tag FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE
 )
--- src/main/resources/db/migration/V4__Add_task_comment_table.sql
+++ src/main/resources/db/migration/V4__Add_task_comment_table.sql
@@ -1,7 +1,7 @@
 -- noinspection SqlResolve
 CREATE TABLE task_comment (
   id BIGSERIAL PRIMARY KEY,
-  task_id BIGINT REFERENCES task(id),
+  task_id BIGINT REFERENCES task(id) ON DELETE CASCADE,
   comment_text VARCHAR(10000) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP
