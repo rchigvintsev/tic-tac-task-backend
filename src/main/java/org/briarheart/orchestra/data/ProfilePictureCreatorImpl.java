0ca928ca517ad31148cbb7024913ef66661960aa
Roman Chigvintsev <rchigvintsev@gmail.com>
2021-08-20 13:52:08 +0300

    Use separate environment properties to configure R2DBC connection

--- src/main/java/org/briarheart/orchestra/data/ProfilePictureCreatorImpl.java
+++ src/main/java/org/briarheart/orchestra/data/ProfilePictureCreatorImpl.java
@@ -2,12 +2,9 @@ package org.briarheart.orchestra.data;
 
 import io.jsonwebtoken.lang.Assert;
 import org.briarheart.orchestra.model.ProfilePicture;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 /**
  * @author Roman Chigvintsev
@@ -16,25 +13,23 @@ import java.util.Objects;
 public class ProfilePictureCreatorImpl implements ProfilePictureCreator {
     @SuppressWarnings("SqlResolve")
     private static final String SQL_CREATE_PROFILE_PICTURE = "INSERT INTO profile_picture (user_id, data, type) "
-            + "VALUES (?, ?, ?)";
+            + "VALUES (:userId, :data, :type)";
 
-    private final JdbcTemplate jdbcTemplate;
+    private final DatabaseClient databaseClient;
 
-    public ProfilePictureCreatorImpl(JdbcTemplate jdbcTemplate) {
-        Assert.notNull(jdbcTemplate, "JDBC template must not be null");
-        this.jdbcTemplate = jdbcTemplate;
+    public ProfilePictureCreatorImpl(DatabaseClient databaseClient) {
+        Assert.notNull(databaseClient, "Database client must not be null");
+        this.databaseClient = databaseClient;
     }
 
     @Override
     public Mono<ProfilePicture> create(ProfilePicture picture) {
-        ProfilePicture result = jdbcTemplate.execute(SQL_CREATE_PROFILE_PICTURE,
-                (PreparedStatementCallback<ProfilePicture>) preparedStatement -> {
-                    preparedStatement.setLong(1, picture.getUserId());
-                    preparedStatement.setBytes(2, picture.getData());
-                    preparedStatement.setString(3, picture.getType());
-                    preparedStatement.executeUpdate();
-                    return picture;
-                });
-        return Mono.just(Objects.requireNonNull(result));
+        return databaseClient.sql(SQL_CREATE_PROFILE_PICTURE)
+                .bind("userId", picture.getUserId())
+                .bind("data", picture.getData())
+                .bind("type", picture.getType())
+                .fetch()
+                .first()
+                .map(result -> picture);
     }
 }
--- src/main/java/org/briarheart/orchestra/data/TaskTagRelationCreatorImpl.java
+++ src/main/java/org/briarheart/orchestra/data/TaskTagRelationCreatorImpl.java
@@ -2,12 +2,9 @@ package org.briarheart.orchestra.data;
 
 import io.jsonwebtoken.lang.Assert;
 import org.briarheart.orchestra.model.TaskTagRelation;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 /**
  * @author Roman Chigvintsev
@@ -15,26 +12,23 @@ import java.util.Objects;
 @Component
 public class TaskTagRelationCreatorImpl implements TaskTagRelationCreator {
     @SuppressWarnings("SqlResolve")
-    private static final String SQL_CREATE_TASK_TAG_RELATION = "INSERT INTO tasks_tags (task_id, tag_id) VALUES (?, ?)";
+    private static final String SQL_CREATE_TASK_TAG_RELATION = "INSERT INTO tasks_tags (task_id, tag_id) "
+            + "VALUES (:taskId, :tagId)";
 
-    private final JdbcTemplate jdbcTemplate;
+    private final DatabaseClient databaseClient;
 
-    public TaskTagRelationCreatorImpl(JdbcTemplate jdbcTemplate) {
-        Assert.notNull(jdbcTemplate, "JDBC template must not be null");
-        this.jdbcTemplate = jdbcTemplate;
+    public TaskTagRelationCreatorImpl(DatabaseClient databaseClient) {
+        Assert.notNull(databaseClient, "Database client must not be null");
+        this.databaseClient = databaseClient;
     }
 
     @Override
     public Mono<TaskTagRelation> create(Long taskId, Long tagId) {
-        return Mono.fromCallable(() -> {
-            TaskTagRelation result = jdbcTemplate.execute(SQL_CREATE_TASK_TAG_RELATION,
-                    (PreparedStatementCallback<TaskTagRelation>) preparedStatement -> {
-                        preparedStatement.setLong(1, taskId);
-                        preparedStatement.setLong(2, tagId);
-                        preparedStatement.executeUpdate();
-                        return new TaskTagRelation(taskId, tagId);
-                    });
-            return Objects.requireNonNull(result);
-        });
+        return databaseClient.sql(SQL_CREATE_TASK_TAG_RELATION)
+                .bind("taskId", taskId)
+                .bind("tagId", tagId)
+                .fetch()
+                .first()
+                .map(result -> new TaskTagRelation(taskId, tagId));
     }
 }
