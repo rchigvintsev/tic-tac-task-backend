41e6aa4cdcefcc6e6107ce88b9f43c0ff2c7181c
Roman Chigvintsev <rchigvintsev@gmail.com>
2022-02-08 11:17:37 +0300

    Set default value of completed_at field to yesterday

--- src/main/resources/db/migration/V36__Add_completed_at_field_to_task_table.sql
+++ src/main/resources/db/migration/V36__Add_completed_at_field_to_task_table.sql
@@ -1,2 +1,4 @@
--- noinspection SqlResolve
+-- noinspection SqlResolveForFile
+
 ALTER TABLE task ADD completed_at TIMESTAMP;
+UPDATE task SET completed_at = (NOW() AT TIME ZONE 'utc') - INTERVAL '1 DAY';
