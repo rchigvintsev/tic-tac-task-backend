99e8a0a5c7271e611f7c39b4af2d53a1637722ad
Roman Chigvintsev <rchigvintsev@gmail.com>
2022-05-01 13:00:26 +0300

    Use JOOQ to generate SQL to select tasks

--- src/main/java/org/briarheart/tictactask/task/CustomizedTaskRepositoryImpl.java
+++ src/main/java/org/briarheart/tictactask/task/CustomizedTaskRepositoryImpl.java
@@ -1,20 +1,31 @@
 package org.briarheart.tictactask.task;
 
 import io.jsonwebtoken.lang.Assert;
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
 import org.briarheart.tictactask.user.User;
 import org.briarheart.tictactask.util.Pageables;
+import org.jooq.*;
+import org.jooq.impl.DSL;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.data.relational.core.query.Criteria;
-import org.springframework.data.relational.core.query.Query;
+import org.springframework.data.r2dbc.core.ReactiveDataAccessStrategy;
+import org.springframework.data.relational.core.sql.IdentifierProcessing;
+import org.springframework.data.relational.core.sql.SqlIdentifier;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.table;
 
 @Component
 public class CustomizedTaskRepositoryImpl implements CustomizedTaskRepository {
@@ -29,7 +40,19 @@ public class CustomizedTaskRepositoryImpl implements CustomizedTaskRepository {
     public Mono<Long> count(GetTasksRequest request, User user) {
         Assert.notNull(request, "Request must not be null");
         Assert.notNull(user, "User must not be null");
-        return entityTemplate.count(Query.query(buildCriteria(request, user)), Task.class);
+
+        DSLContext create = DSL.using(SQLDialect.POSTGRES);
+        SelectConditionStep<Record1<Integer>> query = create.selectCount()
+                .from(table("task"))
+                .where(field("user_id").eq(user.getId()));
+        Condition statusCondition = DSL.noCondition();
+        for (TaskStatus status : request.getStatuses()) {
+            statusCondition = statusCondition.or(getTaskStatusCondition(status, request));
+        }
+        query = query.and(statusCondition);
+
+        DatabaseClient.GenericExecuteSpec executeSpec = createExecuteSpec(query);
+        return executeSpec.map((row, rowMetadata) -> row.get(0, Long.class)).one();
     }
 
     @Override
@@ -37,92 +60,95 @@ public class CustomizedTaskRepositoryImpl implements CustomizedTaskRepository {
         Assert.notNull(request, "Request must not be null");
         Assert.notNull(user, "User must not be null");
 
-        Criteria criteria = buildCriteria(request, user);
-        long offset = Pageables.getOffset(pageable);
-        Integer limit = Pageables.getLimit(pageable);
-
-        Query query = Query.query(criteria).offset(offset).sort(Sort.by("created_at").ascending());
-        if (limit != null) {
-            query = query.limit(limit);
-        }
-        return entityTemplate.select(Task.class).matching(query).all();
-    }
-
-    private Criteria buildCriteria(GetTasksRequest request, User user) {
-        Criteria criteria = Criteria.where("user_id").is(user.getId());
-        Criteria statusCriteria = Criteria.empty();
+        List<Field<Object>> taskFields = getAllFields();
+        DSLContext create = DSL.using(SQLDialect.POSTGRES);
+        Query query = create.select(taskFields)
+                .from(table("task"))
+                .where(field("user_id").eq(user.getId()));
+        Condition statusCondition = DSL.noCondition();
         for (TaskStatus status : request.getStatuses()) {
-            statusCriteria = statusCriteria.or(buildCriteriaForTaskStatus(status, request));
+            statusCondition = statusCondition.or(getTaskStatusCondition(status, request));
         }
-        return criteria.and(statusCriteria);
+        query = ((SelectConditionStep<?>) query).and(statusCondition)
+                .orderBy(field("created_at").desc())
+                .offset(Pageables.getOffset(pageable));
+
+        Integer limit = Pageables.getLimit(pageable);
+        if (limit != null) {
+            query = ((SelectLimitAfterOffsetStep<?>) query).limit(limit);
+        }
+
+        DatabaseClient.GenericExecuteSpec executeSpec = createExecuteSpec(query);
+        BiFunction<Row, RowMetadata, Task> rowMapper = entityTemplate.getDataAccessStrategy().getRowMapper(Task.class);
+        return executeSpec.map((rowMapper)).all();
     }
 
-    private Criteria buildCriteriaForTaskStatus(TaskStatus status, GetTasksRequest request) {
-        if (status == TaskStatus.UNPROCESSED) {
-            return Criteria.where("status").is(TaskStatus.UNPROCESSED);
-        }
+    @SuppressWarnings("deprecation")
+    private List<Field<Object>> getAllFields() {
+        ReactiveDataAccessStrategy dataAccessStrategy = entityTemplate.getDataAccessStrategy();
+        List<SqlIdentifier> taskColumns = dataAccessStrategy.getAllColumns(Task.class);
+        return taskColumns.stream()
+                .map(column -> field(column.toSql(IdentifierProcessing.ANSI)))
+                .toList();
+    }
 
+    private Condition getTaskStatusCondition(TaskStatus status, GetTasksRequest request) {
+        Condition condition = field("status").eq(status.name());
         if (status == TaskStatus.PROCESSED || status == TaskStatus.COMPLETED) {
-            Criteria criteria = Criteria.where("status").is(status).and(buildDeadlineCriteria(request));
-
             if (status == TaskStatus.COMPLETED) {
-                Set<TaskStatus> previousStatuses = request.getStatuses().stream()
+                Set<String> previousStatuses = request.getStatuses().stream()
                         .filter(s -> s != TaskStatus.COMPLETED)
+                        .map(TaskStatus::name)
                         .collect(Collectors.toSet());
                 if (!previousStatuses.isEmpty()) {
-                    criteria = criteria.and("previous_status").in(previousStatuses);
+                    condition = condition.and(field("previous_status").in(previousStatuses));
                 }
 
                 LocalDateTime completedAtFrom = request.getCompletedAtFrom();
-                LocalDateTime completedAtTo = request.getCompletedAtTo();
+                if (completedAtFrom != null) {
+                    condition = condition.and(field("completed_at").ge(completedAtFrom));
+                }
 
-                if (completedAtFrom != null || completedAtTo != null) {
-                    if (completedAtFrom != null) {
-                        criteria = criteria.and("completed_at").greaterThanOrEquals(completedAtFrom);
-                    }
-                    if (completedAtTo != null) {
-                        criteria = criteria.and("completed_at").lessThanOrEquals(completedAtTo);
-                    }
+                LocalDateTime completedAtTo = request.getCompletedAtTo();
+                if (completedAtTo != null) {
+                    condition = condition.and(field("completed_at").le(completedAtTo));
                 }
             }
-
-            return criteria;
+            condition = condition.and(getTaskDeadlineCondition(request));
         }
-
-        return Criteria.empty();
+        return condition;
     }
 
-    private Criteria buildDeadlineCriteria(GetTasksRequest request) {
+    private Condition getTaskDeadlineCondition(GetTasksRequest request) {
         if (request.isWithoutDeadline()) {
-            return Criteria.where("deadline_date").isNull().and("deadline_date_time").isNull();
+            return field("deadline_date").isNull().and(field("deadline_date_time").isNull());
         }
 
-        Criteria deadlineCriteria = Criteria.empty();
-        Criteria deadlineDateCriteria = Criteria.empty();
-
-        if (request.getDeadlineDateFrom() != null) {
-            deadlineDateCriteria = deadlineDateCriteria.and("deadline_date")
-                    .greaterThanOrEquals(request.getDeadlineDateFrom());
+        Condition dateCondition = DSL.noCondition();
+        LocalDate deadlineDateFrom = request.getDeadlineDateFrom();
+        if (deadlineDateFrom != null) {
+            dateCondition = dateCondition.and(field("deadline_date").ge(deadlineDateFrom));
+        }
+        LocalDate deadlineDateTo = request.getDeadlineDateTo();
+        if (deadlineDateTo != null) {
+            dateCondition = dateCondition.and(field("deadline_date").le(deadlineDateTo));
         }
 
-        if (request.getDeadlineDateTo() != null) {
-            deadlineDateCriteria = deadlineDateCriteria.and("deadline_date")
-                    .lessThanOrEquals(request.getDeadlineDateTo());
+        Condition dateTimeCondition = DSL.noCondition();
+        LocalDateTime deadlineDateTimeFrom = request.getDeadlineDateTimeFrom();
+        if (deadlineDateTimeFrom != null) {
+            dateTimeCondition = dateTimeCondition.and(field("deadline_date_time").ge(deadlineDateTimeFrom));
+        }
+        LocalDateTime deadlineDateTimeTo = request.getDeadlineDateTimeTo();
+        if (deadlineDateTimeTo != null) {
+            dateTimeCondition = dateTimeCondition.and(field("deadline_date_time").le(deadlineDateTimeTo));
         }
 
-        deadlineCriteria = deadlineCriteria.or(deadlineDateCriteria);
-        Criteria deadlineDateTimeCriteria = Criteria.empty();
+        return dateCondition.or(dateTimeCondition);
+    }
 
-        if (request.getDeadlineDateTimeFrom() != null) {
-            deadlineDateTimeCriteria = deadlineDateTimeCriteria.and("deadline_date_time")
-                    .greaterThanOrEquals(request.getDeadlineDateTimeFrom());
-        }
-
-        if (request.getDeadlineDateTimeTo() != null) {
-            deadlineDateTimeCriteria = deadlineDateTimeCriteria.and("deadline_date_time")
-                    .lessThanOrEquals(request.getDeadlineDateTimeTo());
-        }
-
-        return deadlineCriteria.or(deadlineDateTimeCriteria);
+    private DatabaseClient.GenericExecuteSpec createExecuteSpec(Query query) {
+        DatabaseClient databaseClient = entityTemplate.getDatabaseClient();
+        return databaseClient.sql(query.toString());
     }
 }
